c9eb531cd5bdba74379057102ea927ae8335f811
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-03-10 17:23:44 +0300

    updated

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -17,8 +17,12 @@ interface AuthState {
 
 // Get stored user from localStorage
 const getStoredUser = (): null | User => {
-  const storedUser = localStorage.getItem('user');
-  return storedUser ? JSON.parse(storedUser) : null;
+  try {
+    const storedUser = localStorage.getItem('user');
+    return storedUser ? JSON.parse(storedUser) : null;
+  } catch {
+    return null;
+  }
 };
 
 const initialState: AuthState = {
