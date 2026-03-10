71bb482291a9cc66e47bbb2d88a9ee4e504b0062
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-03-10 15:46:42 +0300

    done

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const API_URL = import.meta.env.VITE_API_URL;
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://ecommerce-backend-iqft.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL 
 
 export const signUpUser = async (userData: any) => {
   const response = await axios.post(`${API_URL}/api/users/`, userData);
